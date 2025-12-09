7c28700e0672462b2e7bcc7c24aff06337020e54
vansh-cryp <vanshgusain920@gmail.com>
2025-12-09 12:58:58 +0530

    corse error is handeled

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // using proxy to avoid CORS issue
+  // server:{
+  //   proxy:{
+  //     "/api":{
+  //       //url of backend server
+  //       target:"http://localhost:4000",
+  //       changeOrigin:true,
+  //     }
+  //   }
+  // }
+
+  //we will be doing it by back end itself as it will be useful in production also
 })
--- server/index.js
+++ server/index.js
@@ -6,7 +6,12 @@ const PORT = 4000;
 
 //so that data is accepted in json
 app.use(express.json());
-app.use(cors())
+app.use(cors(
+    // url in this array will be accepted by the server
+    { origin:['http://localhost:5174','http://localhost:5173','http://localhost:3000']
+        ,credentials:true
+     }
+))
 
 app.get("/api/message", (req, res) => {res.json({ message: "Hello from Vansh's server!" })});
 
